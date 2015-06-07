cd965db1c73a88884d8f901ee5e5ae81d59c18d0
lucialoto <lucialoto@hotmail.com.ar>
2015-06-07 00:09:02 -0300

    crud docente

---
+++ src/UNTDF/ReservasAulasBundle/Controller/DocenteController.php
@@ -0,0 +1,224 @@
+<?php
+
+namespace UNTDF\ReservasAulasBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use UNTDF\ReservasAulasBundle\Entity\Docente;
+use UNTDF\ReservasAulasBundle\Form\DocenteType;
+
+/**
+ * Docente controller.
+ *
+ */
+class DocenteController extends Controller
+{
+
+    /**
+     * Lists all Docente entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('UNTDFReservasAulasBundle:Docente')->findAll();
+
+        return $this->render('UNTDFReservasAulasBundle:Docente:index.html.twig', array(
+            'entities' => $entities,
+        ));
+    }
+    /**
+     * Creates a new Docente entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Docente();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('docente_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('UNTDFReservasAulasBundle:Docente:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Docente entity.
+     *
+     * @param Docente $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Docente $entity)
+    {
+        $form = $this->createForm(new DocenteType(), $entity, array(
+            'action' => $this->generateUrl('docente_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Docente entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Docente();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('UNTDFReservasAulasBundle:Docente:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Docente entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UNTDFReservasAulasBundle:Docente')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Docente entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('UNTDFReservasAulasBundle:Docente:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Docente entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UNTDFReservasAulasBundle:Docente')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Docente entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('UNTDFReservasAulasBundle:Docente:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Docente entity.
+    *
+    * @param Docente $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Docente $entity)
+    {
+        $form = $this->createForm(new DocenteType(), $entity, array(
+            'action' => $this->generateUrl('docente_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Docente entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UNTDFReservasAulasBundle:Docente')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Docente entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('docente_edit', array('id' => $id)));
+        }
+
+        return $this->render('UNTDFReservasAulasBundle:Docente:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Docente entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('UNTDFReservasAulasBundle:Docente')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Docente entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('docente'));
+    }
+
+    /**
+     * Creates a form to delete a Docente entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('docente_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
---
+++ src/UNTDF/ReservasAulasBundle/Form/DocenteType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace UNTDF\ReservasAulasBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DocenteType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('dni')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UNTDF\ReservasAulasBundle\Entity\Docente'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'untdf_reservasaulasbundle_docente';
+    }
+}
