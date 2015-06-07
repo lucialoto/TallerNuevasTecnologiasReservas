6aec357e7e3067b90b80354d2ba6ca8e82dbed2c
lucialoto <lucialoto@hotmail.com.ar>
2015-06-07 00:52:50 -0300

    crud carrera y tipo actividad

---
+++ src/UNTDF/ReservasAulasBundle/Entity/Carrera.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace UNTDF\ReservasAulasBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Carrera
+ * @ORM\Table(name="carrera")
+ * @ORM\Entity
+ */
+class Carrera
+{
+    /**
+     * @var integer
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(name="nombre", type="string", length=100)
+     */
+    private $nombre;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Carrera
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
---
+++ src/UNTDF/ReservasAulasBundle/Form/TipoActividadType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace UNTDF\ReservasAulasBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TipoActividadType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('cantidadHoras')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UNTDF\ReservasAulasBundle\Entity\TipoActividad'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'untdf_reservasaulasbundle_tipoactividad';
+    }
+}
