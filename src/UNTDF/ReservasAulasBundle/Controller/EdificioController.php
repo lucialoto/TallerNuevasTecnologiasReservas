4102c6ec5c2f354754ef5be4e9df66060219fd35
Maximiliano Sampirisi <msampirisi@gmail.com>
2015-06-08 18:29:55 -0300

    Correcciones en general. security Nueva clase edificio

--- src/UNTDF/ReservasAulasBundle/Controller/EdificioController.php
+++ src/UNTDF/ReservasAulasBundle/Controller/EdificioController.php
@@ -2,144 +2,223 @@
 
 namespace UNTDF\ReservasAulasBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use UNTDF\ReservasAulasBundle\Entity\Edificio;
+use UNTDF\ReservasAulasBundle\Form\EdificioType;
 
+/**
+ * Edificio controller.
+ *
+ */
 class EdificioController extends Controller
 {
+
+    /**
+     * Lists all Edificio entities.
+     *
+     */
     public function indexAction()
     {
-	$edificios = $this->getDoctrine()->getRepository('UNTDFReservasAulasBundle:Edificio')->findAll();
-        return $this->render('UNTDFReservasAulasBundle:Edificio:index.html.twig', array('edificios' => $edificios ));
-    }
-    
-    public function showAction($id)
-    {
-        $edificio  = $this->getDoctrine()->getRepository('UNTDFReservasAulasBundle:Edificio')->find($id);
+        $em = $this->getDoctrine()->getManager();
 
-        if (!$edificio) {
-            throw $this->createNotFoundException('(show) No hay edificios con ese codigo : '.$id);
-        }
-        
-        return $this->render('UNTDFReservasAulasBundle:Edificio:show.html.twig', array('edificio' => $edificio ));
-    }
+        $entities = $em->getRepository('UNTDFReservasAulasBundle:Edificio')->findAll();
 
-    public function removeAction($id)
-    {
-
-        $edificio  = $this->getDoctrine()->getRepository('UNTDFReservasAulasBundle:Edificio')->find($id);
-
-        if (!$edificio) {
-            throw $this->createNotFoundException('(remove) No hay edificios con ese codigo : '.$id);
-        }else{
-            $em = $this->getDoctrine()->getEntityManager();
-            $em->remove($edificio);
-            $em->flush();
-        }
-        
-        return $this->redirect($this->generateUrl('edificios'));
-        
-        //return $this->render('UNTDFReservasAulasBundle:Edificio:show.html.twig', array('edificio' => $edificio ));
-
-    }
-
-    public function createAction(Request $request){
-        
-        $edificio = new Edificio();
-
-        $form = $this->createFormBuilder($edificio)
-                ->add('nombre', 'text')
-                ->add('grabar', 'submit', ['label' => 'Crear Edificio (crear)', 'attr' => ['class' => 'btn btn-success']])
-                ->setAction($this->generateUrl('edificio/crear'))
-                ->setMethod('POST')
-                ->getForm();
-
-        $form->handleRequest($request);
-        
-        if($form->isValid()){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($edificio);
-            $em->flush();
-            
-            return $this->redirect($this->generateUrl('edificio/ver',array('id' => $edificio->getId())));
-        }
-        
-        return $this->render('UNTDFReservasAulasBundle:Edificio:new.html.twig', array(
-            'form' => $form->createView(),
+        return $this->render('UNTDFReservasAulasBundle:Edificio:index.html.twig', array(
+            'entities' => $entities,
         ));
-        
     }
-    
+    /**
+     * Creates a new Edificio entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Edificio();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('edificio_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('UNTDFReservasAulasBundle:Edificio:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Edificio entity.
+     *
+     * @param Edificio $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Edificio $entity)
+    {
+        $form = $this->createForm(new EdificioType(), $entity, array(
+            'action' => $this->generateUrl('edificio_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Edificio entity.
+     *
+     */
     public function newAction()
     {
-        
-        $edificio = new Edificio();
+        $entity = new Edificio();
+        $form   = $this->createCreateForm($entity);
 
-        $form = $this->createFormBuilder($edificio)
-                ->add('nombre', 'text')
-                ->add('grabar', 'submit', ['label' => 'Crear Edificio (crear)', 'attr' => ['class' => 'btn btn-success']])
-                ->setAction($this->generateUrl('edificio/crear'))
-                ->setMethod('POST')
-                ->getForm();
-        
         return $this->render('UNTDFReservasAulasBundle:Edificio:new.html.twig', array(
-            'form' => $form->createView(),
+            'entity' => $entity,
+            'form'   => $form->createView(),
         ));
     }
 
+    /**
+     * Finds and displays a Edificio entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UNTDFReservasAulasBundle:Edificio')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Edificio entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('UNTDFReservasAulasBundle:Edificio:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Edificio entity.
+     *
+     */
     public function editAction($id)
     {
-        
-        $edificio  = $this->getDoctrine()->getRepository('UNTDFReservasAulasBundle:Edificio')->find($id);
+        $em = $this->getDoctrine()->getManager();
 
-        if (!$edificio) {
-            throw $this->createNotFoundException('(edit) No se pudo acceder a la entidad Edificio con el id '.$id);
-        }else{
-            $form = $this->createFormBuilder($edificio)
-                    ->add('nombre', 'text')
-                    ->add('grabar', 'submit')
-                    ->setAction($this->generateUrl('edificio/update', array('id' => $edificio->getId())))
-                    ->setMethod('POST')
-                    ->getForm();
+        $entity = $em->getRepository('UNTDFReservasAulasBundle:Edificio')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Edificio entity.');
         }
-        
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
         return $this->render('UNTDFReservasAulasBundle:Edificio:edit.html.twig', array(
-            'form' => $form->createView(),
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
         ));
     }
-    
-    public function updateAction(Request $request, $id){
-        
-        $edificio  = $this->getDoctrine()->getRepository('UNTDFReservasAulasBundle:Edificio')->find($id);
 
-        if (!$edificio) {
-            throw $this->createNotFoundException('(update) No se pudo acceder a la entidad Edificio con el id '.$id);
-        }else{
-            $form = $this->createFormBuilder($edificio)
-                    ->add('nombre', 'text')
-                    ->add('grabar', 'submit')
-                    ->setAction($this->generateUrl('edificio/update', array('id' => $edificio->getId())))
-                    ->setMethod('POST')
-                    ->getForm();
+    /**
+    * Creates a form to edit a Edificio entity.
+    *
+    * @param Edificio $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Edificio $entity)
+    {
+        $form = $this->createForm(new EdificioType(), $entity, array(
+            'action' => $this->generateUrl('edificio_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Edificio entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UNTDFReservasAulasBundle:Edificio')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Edificio entity.');
         }
 
-        $form->handleRequest($request);
-        
-        if($form->isValid()){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($edificio);
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
             $em->flush();
-            
-            return $this->redirect($this->generateUrl('edificio/ver',array('id' => $edificio->getId())));
+
+            return $this->redirect($this->generateUrl('edificio_edit', array('id' => $id)));
         }
-        
+
         return $this->render('UNTDFReservasAulasBundle:Edificio:edit.html.twig', array(
-            'form' => $form->createView(),
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
         ));
-        
+    }
+    /**
+     * Deletes a Edificio entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('UNTDFReservasAulasBundle:Edificio')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Edificio entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('edificio'));
     }
 
-    
+    /**
+     * Creates a form to delete a Edificio entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('edificio_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
 }
