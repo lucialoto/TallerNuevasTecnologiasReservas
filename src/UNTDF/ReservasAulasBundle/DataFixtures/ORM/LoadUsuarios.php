753a34fbe32e7065f0751edc385606accdd2b4a5
Maximiliano Sampirisi <msampirisi@gmail.com>
2015-07-08 22:59:53 -0300

    Fixture Usuarios Modificar la forma de generarlos para utilizar las funciones propias de fos-user Agregue la carga del password en formato plano, asi, la rutina de fos_user genera el hash Agregue la carga del rol.

--- src/UNTDF/ReservasAulasBundle/DataFixtures/ORM/LoadUsuarios.php
+++ src/UNTDF/ReservasAulasBundle/DataFixtures/ORM/LoadUsuarios.php
@@ -6,11 +6,23 @@ namespace UNTDF\ReservasAulasBundle\DataFixtures\ORM;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-use UNTDF\ReservasAulasBundle\Entity\User;
+// use UNTDF\ReservasAulasBundle\Entity\User;
 
-class LoadUsuarios extends AbstractFixture implements OrderedFixtureInterface
+class LoadUsuarios extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
+
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
     /**
      * {@inheritDoc}
      */
@@ -20,29 +32,36 @@ class LoadUsuarios extends AbstractFixture implements OrderedFixtureInterface
             array( 
                 'nombre' => 'admin',
                 'password' => 'password',
-                'email' => 'admin@untdf.edu.ar'
+                'email' => 'admin@untdf.edu.ar',
+                'rol' => 'ROLE_SUPER_ADMIN'
             ),
             array( 
                 'nombre' => 'usuario',
                 'password' => 'password',
-                'email' => 'usuario@untdf.edu.ar'
+                'email' => 'usuario@untdf.edu.ar',
+                'rol' => 'ROLE_USER'
             ),
         );
     
+        $userManager = $this->container->get('fos_user.user_manager');
+                
         foreach( $usuarios as $usuarioValue ){
                         
-            $usuario = new User();
+            $usuario = $userManager->createUser();
             $usuario->setUsername($usuarioValue['nombre']);
-            $usuario->setPassword($usuarioValue['password']);
+            $usuario->setPlainPassword($usuarioValue['password']);
             $usuario->setEmail($usuarioValue['email']);
+            $usuario->setRoles(array($usuarioValue['rol']));
             $usuario->setEnabled(true);
-            
+                    
             $this->addReference( 'usuario-' . $usuarioValue['nombre'], $usuario);
                         
-            $manager->persist($usuario);
+            $userManager->updateUser($usuario, true);
+            
+            //$manager->persist($usuario);
         }    
         
-        $manager->flush();
+        //$manager->flush();
     }
     
     public function getOrder()
